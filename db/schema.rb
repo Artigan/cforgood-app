bb04ae1f4586c884d98b7e92de2cae40bc9c0a98
Alexandre Cordani <alcordani@gmail.com>
2015-07-09 12:45:45 +0200

    ajout de scroll smooth

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706080516) do
+ActiveRecord::Schema.define(version: 20150706131938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "businesses", force: :cascade do |t|
+    t.string   "name"
+    t.string   "category"
+    t.string   "street"
+    t.string   "zipcode"
+    t.string   "city"
+    t.string   "url"
+    t.string   "telephone"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
