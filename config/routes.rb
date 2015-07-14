8f6867d830bdd4d78fc8506f760662690fc43e18
allan floury <allan.floury@gmail.com>
2015-07-14 18:12:24 +0200

    pages controller + link

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'pages/business', to: 'pages#business'
+  get 'pages/charity', to: 'pages#charity'
+  get 'pages/vision', to: 'pages#vision'
 end
