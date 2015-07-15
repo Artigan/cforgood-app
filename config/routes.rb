ea752faf00bce936d16b1118fa2117938964b85b
Nasreddine BOUKEHIL <boukehil.nasreddine@gmail.com>
2015-07-15 19:08:54 +0200

    add routes for static pages & toastr avoided

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
+
   devise_for :users
+
+  get 'pages/home',     to: 'pages#home'
   get 'pages/business', to: 'pages#business'
-  get 'pages/charity', to: 'pages#charity'
-  get 'pages/vision', to: 'pages#vision'
+  get 'pages/charity',  to: 'pages#charity'
+  get 'pages/vision',   to: 'pages#vision'
 end
