dade6ef78a8ac2681167be77850a56bc6eaa07af
allan floury <allan.floury@gmail.com>
2015-07-19 19:14:33 +0200

    change name page vision + modif pages business charity how it works

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'pages/business', to: 'pages#business'
   get 'pages/charity',  to: 'pages#charity'
-  get 'pages/vision',   to: 'pages#vision'
+  get 'pages/how_it_works',   to: 'pages#how_it_works'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
