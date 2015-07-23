bfffa9f942acd958512448a56dd70e5648758308
allan floury <allan.floury@gmail.com>
2015-07-23 18:22:54 +0200

    change home design + navbar + ambassador

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,8 +1,8 @@
 $(function(){
   $(window).scroll(function(e){
-    if ($(this).scrollTop() > 0) {
+    if ($(this).scrollTop() > 200) {
       $(".navbar-wagon").css({
-        "background": "rgba(0, 0, 0, 0.5)",
+        "background": "rgba(54, 211, 218, 1)",
         "box-shadow": "0 0 2px black"
       });
     }
