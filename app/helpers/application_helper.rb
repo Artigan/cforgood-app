e1caf06f17b3b513165c305558b23dff81787855
allan floury <allan.floury@gmail.com>
2015-07-26 16:05:55 +0200

    fix bug responsive pages business + charity

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,17 @@ module ApplicationHelper
       "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identicon&s=30"
     end
   end
+
+  #def title
+    #content_for?(:title) ? content_for(:title) : DEFAULT_META['default_title']
+  #end
+
+  #def meta_description
+    #content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META['meta_description']
+  #end
+
+  #def meta_image
+    #content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META['meta_image']
+  #end
+
 end
