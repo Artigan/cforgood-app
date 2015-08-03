6e1250a718b444797026e63dabc2858d29bfdbdf
allan floury <allan.floury@gmail.com>
2015-08-03 16:30:40 +0200

    add cforgood gradient + modal for betafamily

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -2,8 +2,11 @@ $(function() {
   $(window).scroll(function(e) {
     if ($(this).scrollTop() > 200) {
       $(".navbar-wagon").css( {
-        "background": "rgb(54, 211, 218)",
-        //"background-image": 'url("../images/navbar1.png")',
+        "background": "-webkit-linear-gradient(90deg, #00C9FF 10%, #80F2A4 90%)", /* Chrome 10+, Saf5.1+ */
+        "background": "-moz-linear-gradient(90deg, #00C9FF 10%, #80F2A4 90%)", /* FF3.6+ */
+        "background": "-ms-linear-gradient(90deg, #00C9FF 10%, #80F2A4 90%)", /* IE10 */
+        "background": "-o-linear-gradient(90deg, #00C9FF 10%, #80F2A4 90%)", /* Opera 11.10+ */
+        "background": "linear-gradient(90deg, #00C9FF 10%, #80F2A4 90%)", /* W3C */
         "box-shadow": "0 0 2px black"
       });
     }
@@ -16,7 +19,7 @@ $(function() {
     else {
       $(".navbar-wagon").css({
         "background": "transparent",
-        "box-shadow": "0 0 0px transparent"
+        "box-shadow": "0 0 0px transparent",
       });
     }
   });
