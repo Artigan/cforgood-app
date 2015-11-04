c1e1417588fe8824a1a43777de72736480d65159
cedricmenteau <me.cedric@gmail.com>
2015-11-04 19:56:00 +0100

    Refresh navbar 1/2

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,26 +1,13 @@
-$(function() {
-  $(window).scroll(function(e) {
-    if ($(this).scrollTop() > 200) {
-      $(".navbar-wagon").css( {
-        "background": "-webkit-linear-gradient(90deg, #00C9FF 10%, #80F2A4 90%)", /* Chrome 10+, Saf5.1+ */
-        "background": "-moz-linear-gradient(90deg, #00C9FF 10%, #80F2A4 90%)", /* FF3.6+ */
-        "background": "-ms-linear-gradient(90deg, #00C9FF 10%, #80F2A4 90%)", /* IE10 */
-        "background": "-o-linear-gradient(90deg, #00C9FF 10%, #80F2A4 90%)", /* Opera 11.10+ */
-        "background": "linear-gradient(90deg, #00C9FF 10%, #80F2A4 90%)", /* W3C */
-        "box-shadow": "0 0 2px black"
-      });
-    }
-    else if ($(this).scrollTop() > 0) {
-      $(".navbar-wagon").css({
-        "background": "rgba(0, 0, 0, 0.4)",
-        "box-shadow": "0 0 0px transparent"
-      });
-    }
-    else {
-      $(".navbar-wagon").css({
-        "background": "transparent",
-        "box-shadow": "0 0 0px transparent",
-      });
-    }
-  });
-})
+$window = $(window);
+$window.scroll(function(){
+  if($window.scrollTop() > 0){
+    $('#navbar').addClass('navbar-scrolled');
+  }else{
+    $('#navbar').removeClass('navbar-scrolled');
+  };
+});
+
+$('.menu-toggle').click(function() {
+  $('burger').toggleClass('menu-checked');
+  $('#navbar').toggleClass('menu-toggled');
+});
