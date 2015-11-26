c4839b70875ae699e90ffe44a67e53127cfa8841
Frederique Petris <frederique.petris@gmail.com>
2015-11-26 11:27:30 +0100

    change path and label_false in simple_form

--- app/controllers/pro/pages_controller.rb
+++ app/controllers/pro/pages_controller.rb
@@ -2,6 +2,8 @@ class Pro::PagesController < ApplicationController
 
   layout 'pro'
 
+  before_action :authenticate_business!
+
   def home
     @business = Business.find(params[:business_id])
     @perks = @business.perks
