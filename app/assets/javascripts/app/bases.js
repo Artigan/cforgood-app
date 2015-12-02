37e3f064b15d9c740da6855a1d7dce2d0c8b4473
cedricmenteau <me.cedric@gmail.com>
2015-12-02 13:56:15 +0100

    Design pro create bon plan

--- app/assets/javascripts/app/bases.js
+++
@@ -1,7 +0,0 @@
-function validate() {
-  if (document.getElementById('radio-check').checked) {
-      alert("checked");
-  } else {
-      alert("You didn't check it! Let me check it for you.");
-  }
-}
