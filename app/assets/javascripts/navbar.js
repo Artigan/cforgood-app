832551f0d6cc382d97d6bb93d5deda7b873df4f8
cedricmenteau <me.cedric@gmail.com>
2016-01-29 18:32:49 +0100

    Add overlay mobile menu and logo on mobile pages

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -10,12 +10,6 @@ if ($(window).width() > 768) {
 };
 
 $(document).ready(function(){
-  $('.mobile-dropdown').click(function(event) {
-    $('.mobile-menu').toggleClass('hidden');
-  });
-});
-
-$(document).ready(function(){
   $('.menu-toggle').click(function(event) {
     $('#navbar').toggleClass('menu-toggled');
     $('#menu-icon').toggleClass('open');
