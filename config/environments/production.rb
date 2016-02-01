a1b3378f7395c13b2d770be3b1074c21d379a071
Frederique Petris <frederique.petris@gmail.com>
2016-02-01 10:33:58 +0100

    add asset pipeline to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,14 +63,13 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-  config.action_controller.asset_host =  host: ENV.fetch("HOST") }
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.asset_host =  host: ENV.fetch("HOST") }
+  config.action_mailer.asset_host =  { host: ENV.fetch("HOST") }
 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
