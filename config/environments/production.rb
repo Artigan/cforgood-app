9a721f38c72fd7e117aae1fec966efeb3929d698
Frederique Petris <frederique.petris@gmail.com>
2016-02-01 10:59:58 +0100

    new test for asset in maile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.asset_host =  { host: ENV.fetch("HOST") }
+  config.action_mailer.asset_host = ENV.fetch("HOST")
 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
