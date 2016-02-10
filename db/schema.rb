a08f9870894dbe0dbff02355fc7c1d2f91a56ba9
cedricmenteau <me.cedric@gmail.com>
2016-02-10 21:33:28 +0100

    Design cards index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204154204) do
+ActiveRecord::Schema.define(version: 20160210160839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160204154204) do
     t.string   "marker_content_type"
     t.integer  "marker_file_size"
     t.datetime "marker_updated_at"
+    t.string   "color"
   end
 
   create_table "businesses", force: :cascade do |t|
@@ -100,6 +101,7 @@ ActiveRecord::Schema.define(version: 20160204154204) do
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
+    t.string   "color"
   end
 
   create_table "causes", force: :cascade do |t|
