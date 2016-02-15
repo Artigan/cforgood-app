fc8e08210ee65015bdbdab8fcead150396b06698
Frederique Petris <frederique.petris@gmail.com>
2016-02-15 12:43:00 +0100

    new map on map_perk_show

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -9,18 +9,18 @@
   def show
     @business = Business.find(params[:id])
 
-    @geojson = Array.new
-    @geojson << {
+    @geojson = {"type" => "FeatureCollection", "features" => []}
+
+    @geojson["features"] << {
       "type": 'Feature',
       "geometry": {
         "type": 'Point',
         "coordinates": [@business.longitude, @business.latitude]
       },
       "properties": {
-        "icon": {
-          "iconUrl": BusinessCategory.find(@business.business_category_id).marker.url,
-          "iconSize": [40, 43]
-        }
+        # "marker-symbol": ("." + business.business_category_id.to_s),
+        "marker-symbol": "monument",
+        # "description": render_to_string(partial: "components/map_box", locals: { business: business })
       }
     }
     respond_to do |format|
