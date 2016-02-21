e30ff29e2c7b4d39d775d826286bd70fa21ed63d
Frederique Petris <frederique.petris@gmail.com>
2016-02-21 23:59:40 +0100

    mail reset-password in french

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -21,7 +21,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Allan @ Cforgood <allan@cforgood.com>'
+  config.mailer_sender = 'Hello @ Cforgood <hello@cforgood.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
