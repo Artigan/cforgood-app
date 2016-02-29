02a8ab9fe4aff3c3ecdc60d49f7b0e37dee4c274
Frederique Petris <frederique.petris@gmail.com>
2016-02-29 14:05:05 +0100

    avatar add format (:thumb)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   def avatar_url(user)
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     if user.picture # si le user est connecté avec fb
-      user.picture  # on affiche img de profile
+      user.picture(:thumb)  # on affiche img de profile
     else          # sinon on utilise gravatar pr son email
       "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identicon&s=30"
     end
