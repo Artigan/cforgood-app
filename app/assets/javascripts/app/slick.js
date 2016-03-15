b9b208f82f9b4d11fa158d871b3ac1190893e53b
cedricmenteau <me.cedric@gmail.com>
2016-03-15 23:19:00 +0100

    Add elements in current user cause

--- app/assets/javascripts/app/slick.js
+++ app/assets/javascripts/app/slick.js
@@ -122,34 +122,7 @@ $('.causes-carrousel').slick({
   arrows:true,
   infinite: false,
   autoplay: false,
-  slidesToShow: 3,
-  variableWidth: true,
-  // responsive: [
-  //   {
-  //     breakpoint: 1024,
-  //     settings: {
-  //       slidesToShow: 1,
-  //       slidesToScroll: 1,
-  //       infinite: false,
-  //       dots: false
-  //     }
-  //   },
-  //   {
-  //     breakpoint: 600,
-  //     settings: {
-  //       slidesToShow: 2,
-  //       slidesToScroll: 1
-  //     }
-  //   },
-  //   {
-  //     breakpoint: 480,
-  //     settings: {
-  //       slidesToShow: 1,
-  //       slidesToScroll: 1
-  //     }
-  //   }
-  //   // You can unslick at a given breakpoint now by adding:
-  //   // settings: "unslick"
-  //   // instead of a settings object
-  // ]
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  variableWidth: true
 });
