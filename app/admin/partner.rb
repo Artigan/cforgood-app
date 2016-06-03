0d63b9707e6f506e69bdbfc9f8c9b4146c98c3e9
Frederique Petris <frederique.petris@gmail.com>
2016-06-04 01:37:54 +0200

    rollback : no card if trial

--- app/admin/partner.rb
+++ app/admin/partner.rb
@@ -5,6 +5,7 @@ ActiveAdmin.register Partner do
     column :name
     column :email
     column :code_partner
+    column :nb_month
     column :created_at
     actions
   end
@@ -13,9 +14,10 @@ ActiveAdmin.register Partner do
       f.input :name
       f.input :email
       f.input :code_partner
+      f.input :nb_month
     end
     f.actions
   end
 
-permit_params :name, :email, :code_partner
+permit_params :name, :email, :code_partner, :nb_month
 end
