c9c4c2975ef097f430aab795ac67a7ee816132ad
Charles Pernet <charlespernet@gmail.com>
2016-10-27 18:57:57 +0200

    Removed migrations :
    - 20150728094211_add_attachment_picture_to_businesses.rb
    - 20150728094247_add_attachment_picture_to_causes.rb
    - 20150728094353_add_attachment_picture_to_users.rb
    - 20150928155019_add_attachment_picture_to_cause_categories.rb
    - 20151129162924_add_attachment_leader_picture_to_businesses.rb
    - 20160106142345_add_attachement_to_business_categories.rb
    - 20160114101134_add_logo_to_causes.rb
    - 20160114222513_add_marker_to_business_categories.rb
    - 20160204154204_add_column_picture_to_perks.rb
    - 20160208133640_add_logo_to_businesses.rb
    - 20160218104858_remove_marker_from_business_categories.rb
    - 20160427142040_rename_promo_to_partner_from_partners.rb
    - 20160714165943_rename_columns_for_migration_cloudinary.rb
    - 20160916081509_remove_column_s3_from_multiple_models.rb

--- db/migrate/20160218104858_remove_marker_from_business_categories.rb
+++
@@ -1,8 +0,0 @@
-class RemoveMarkerFromBusinessCategories < ActiveRecord::Migration
-  def change
-    remove_column :business_categories, :marker_file_name
-    remove_column :business_categories, :marker_content_type
-    remove_column :business_categories, :marker_file_size
-    remove_column :business_categories, :marker_updated_at
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -21,15 +20,14 @@ ActiveRecord::Schema.define(version: 20160930160440) do
     t.text     "body"
     t.string   "resource_id",   null: false
     t.string   "resource_type", null: false
-    t.integer  "author_id"
     t.string   "author_type"
+    t.integer  "author_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
   end
-
-  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
-  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
-  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "business_id"
@@ -39,30 +37,13 @@ ActiveRecord::Schema.define(version: 20160930160440) do
     t.string   "city"
     t.float    "latitude"
     t.float    "longitude"
+    t.boolean  "active",      default: true, null: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-    t.boolean  "active",      default: true, null: false
     t.datetime "start_time"
     t.datetime "end_time"
+    t.index ["business_id"], name: "index_addresses_on_business_id", using: :btree
   end
-
-  add_index "addresses", ["business_id"], name: "index_addresses_on_business_id", using: :btree
-
-  create_table "attachinary_files", force: :cascade do |t|
-    t.integer  "attachinariable_id"
-    t.string   "attachinariable_type"
-    t.string   "scope"
-    t.string   "public_id"
-    t.string   "version"
-    t.integer  "width"
-    t.integer  "height"
-    t.string   "format"
-    t.string   "resource_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "attachinary_files", ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent", using: :btree
 
   create_table "business_categories", force: :cascade do |t|
     t.string   "name"
@@ -111,11 +92,10 @@ ActiveRecord::Schema.define(version: 20160930160440) do
     t.string   "picture"
     t.string   "leader_picture"
     t.string   "logo"
+    t.index ["business_category_id"], name: "index_businesses_on_business_category_id", using: :btree
+    t.index ["email"], name: "index_businesses_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_businesses_on_reset_password_token", unique: true, using: :btree
   end
-
-  add_index "businesses", ["business_category_id"], name: "index_businesses_on_business_category_id", using: :btree
-  add_index "businesses", ["email"], name: "index_businesses_on_email", unique: true, using: :btree
-  add_index "businesses", ["reset_password_token"], name: "index_businesses_on_reset_password_token", unique: true, using: :btree
 
   create_table "cause_categories", force: :cascade do |t|
     t.string   "name"
@@ -153,13 +133,8 @@ ActiveRecord::Schema.define(version: 20160930160440) do
     t.string   "link_video"
     t.string   "picture"
     t.string   "logo"
-    t.boolean  "mailing",                   default: true
-    t.boolean  "tax_receipt",               default: true
-    t.string   "followers"
-    t.string   "heard"
+    t.index ["cause_category_id"], name: "index_causes_on_cause_category_id", using: :btree
   end
-
-  add_index "causes", ["cause_category_id"], name: "index_causes_on_cause_category_id", using: :btree
 
   create_table "partners", force: :cascade do |t|
     t.string   "name"
@@ -178,10 +153,9 @@ ActiveRecord::Schema.define(version: 20160930160440) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.boolean  "done",       default: false, null: false
+    t.index ["cause_id"], name: "index_payments_on_cause_id", using: :btree
+    t.index ["user_id"], name: "index_payments_on_user_id", using: :btree
   end
-
-  add_index "payments", ["cause_id"], name: "index_payments_on_cause_id", using: :btree
-  add_index "payments", ["user_id"], name: "index_payments_on_user_id", using: :btree
 
   create_table "perk_details", force: :cascade do |t|
     t.string   "name"
@@ -194,27 +168,26 @@ ActiveRecord::Schema.define(version: 20160930160440) do
     t.string   "name"
     t.integer  "business_id"
     t.text     "description"
-    t.integer  "times",          default: 0
+    t.integer  "times",             default: 0
     t.datetime "start_date"
     t.datetime "end_date"
-    t.boolean  "active",         default: true,  null: false
+    t.boolean  "active",            default: true,  null: false
     t.string   "perk_code"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.integer  "nb_views",       default: 0
-    t.boolean  "appel",          default: false, null: false
-    t.boolean  "durable",        default: false, null: false
-    t.boolean  "flash",          default: false, null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.integer  "nb_views",          default: 0
+    t.boolean  "appel",             default: false, null: false
+    t.boolean  "durable",           default: false, null: false
+    t.boolean  "flash",             default: false, null: false
     t.integer  "perk_detail_id"
-    t.boolean  "deleted",        default: false, null: false
-    t.boolean  "all_day",        default: false, null: false
+    t.boolean  "deleted",           default: false, null: false
+    t.boolean  "all_day",           default: false, null: false
     t.string   "picture"
     t.string   "text_notification"
     t.boolean  "send_notification", default: false
+    t.index ["business_id"], name: "index_perks_on_business_id", using: :btree
+    t.index ["perk_detail_id"], name: "index_perks_on_perk_detail_id", using: :btree
   end
-
-  add_index "perks", ["business_id"], name: "index_perks_on_business_id", using: :btree
-  add_index "perks", ["perk_detail_id"], name: "index_perks_on_perk_detail_id", using: :btree
 
   create_table "prospects", force: :cascade do |t|
     t.integer  "user_id"
@@ -227,9 +200,8 @@ ActiveRecord::Schema.define(version: 20160930160440) do
     t.boolean  "canvassed",   default: true, null: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.index ["user_id"], name: "index_prospects_on_user_id", using: :btree
   end
-
-  add_index "prospects", ["user_id"], name: "index_prospects_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -276,11 +248,10 @@ ActiveRecord::Schema.define(version: 20160930160440) do
     t.datetime "date_stop_subscription"
     t.string   "picture"
     t.boolean  "ambassador",             default: false
+    t.index ["cause_id"], name: "index_users_on_cause_id", using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
-
-  add_index "users", ["cause_id"], name: "index_users_on_cause_id", using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "uses", force: :cascade do |t|
     t.integer  "user_id"
@@ -291,10 +262,9 @@ ActiveRecord::Schema.define(version: 20160930160440) do
     t.boolean  "like",       default: false
     t.boolean  "unlike",     default: false
     t.boolean  "unused",     default: false
+    t.index ["perk_id"], name: "index_uses_on_perk_id", using: :btree
+    t.index ["user_id"], name: "index_uses_on_user_id", using: :btree
   end
-
-  add_index "uses", ["perk_id"], name: "index_uses_on_perk_id", using: :btree
-  add_index "uses", ["user_id"], name: "index_uses_on_user_id", using: :btree
 
   add_foreign_key "addresses", "businesses"
   add_foreign_key "payments", "causes"
