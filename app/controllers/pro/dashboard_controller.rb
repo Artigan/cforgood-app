c4ff9cf73f8cfe16d0600be5e30b623337674d82
Charles Pernet <charlespernet@gmail.com>
2016-11-10 17:57:20 +0100

    DashboardController#find_business add method to retrieve business called before action

--- app/controllers/pro/dashboard_controller.rb
+++ app/controllers/pro/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class Pro::DashboardController < Pro::ProController
-  
+
   skip_after_action :verify_authorized, only: :set_impersonation
+  before_action :find_business, only: [:dashboard, :profile]
 
   def set_impersonation
     if current_business.supervising?(params[:impersonate_id])
@@ -12,14 +13,19 @@ class Pro::DashboardController < Pro::ProController
   end
 
   def dashboard
-    @business = Business.find(params[:business_id])
     @perks = @business.perks
     authorize @business
   end
 
   def profile
-    @business = Business.find(params[:business_id])
     5.times{ current_business.addresses.build }
     authorize @business
+  end
+
+  private
+
+  def find_business
+    id = session[:impersonate_id] || params[:business_id]
+    @business = Business.find(id)
   end
 end
