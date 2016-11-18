530c43ef856e45d9c14bdbe704db58c04930f620
aurelien galoppe <agaloppe@me.com>
2016-11-18 16:14:19 +0100

    perk index done

--- app/assets/javascripts/app/perk_modal_mobile.js
+++ app/assets/javascripts/app/perk_modal_mobile.js
@@ -13,3 +13,20 @@ $(document).ready(function() {
     $(".perk-modal-mobile").addClass('hidden');
     });
 });
+
+
+
+
+
+$(document).ready(function() {
+    $(".perk-pseudo-modal-link-mobile-member").click(function() {
+    $(".perk-modal-mobile-member").removeClass("hidden");
+    });
+});
+
+
+$(document).ready(function() {
+    $('.perk-modal-close-mobile-member').click(function() {
+    $(".perk-modal-mobile-member").addClass('hidden');
+    });
+});
