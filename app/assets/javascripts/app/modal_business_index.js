3bc3223fff375da9f1a3e458f88972dd79ecda78
aurelien galoppe <agaloppe@me.com>
2016-11-24 14:16:52 +0100

    functionnal search

---
+++ app/assets/javascripts/app/modal_business_index.js
@@ -0,0 +1,23 @@
+
+
+
+$(document).ready(function() {
+  $('#search-in-navbar-mobile').keyup(function(){
+      $(".business-search-index-js").removeClass("hidden");
+  });
+});
+
+
+$(document).ready(function() {
+    $('.business-search-close-button').click(function() {
+    $(".business-search-index-js").addClass('hidden');
+    });
+});
+
+
+$(document).ready(function() {
+    $('.business-search-close-button').click(function() {
+      $('#search-in-navbar-mobile').val('');
+    });
+});
+
