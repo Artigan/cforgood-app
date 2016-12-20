e5b72518cf20e7e439bfafddce5491dcca8216e5
Frederique Petris <frederique.petris@gmail.com>
2016-12-20 12:26:07 +0100

    add missing js

---
+++ app/assets/javascripts/app/perk_modal.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $(".perk-pseudo-modal-link").click(function() {
+    $(".perk-modal").removeClass("hidden");
+    });
+});
+
+
+$(document).ready(function() {
+    $('.perk-modal-close').click(function() {
+    $(".perk-modal").addClass('hidden');
+    });
+});
+
+
+
+
