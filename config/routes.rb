6c160275fa3a9f3e08e07036b749fbdf22e764ef
Charles Pernet <charlespernet@gmail.com>
2017-02-02 18:31:25 +0100

    added route for update member users

--- config/routes.rb
+++ config/routes.rb
@@ -42,7 +42,7 @@ Rails.application.routes.draw do
   end
 
   namespace :member do
-    resources :users, only: [:new, :create] do
+    resources :users, only: [:new, :create, :update] do
       get "dashboard",  to: "dashboard#dashboard"
       get "my_account",  to: "dashboard#my_account"
       get "profile",    to: "dashboard#profile"
