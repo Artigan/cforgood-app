5ce0d000448278415c93ebe18edb6a5fc5e73aa5
Frederique Petris <frederique.petris@gmail.com>
2017-02-23 09:48:25 +0100

    WIP on business perl list and modal for use

--- app/assets/javascripts/app/perk_modal.js
+++ app/assets/javascripts/app/perk_modal.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
-  $(".perk-pseudo-modal-link").click(function() {
-    var classModal = $(this).attr('id');
-    $('.' + classModal).removeClass("hidden");
-    $('.' + classModal).slideDown();
-  });
-});
+// $(document).ready(function() {
+//   $(".perk-pseudo-modal-link").click(function() {
+//     var classModal = $(this).attr('id');
+//     $('.' + classModal).removeClass("hidden");
+//     $('.' + classModal).slideDown();
+//   });
+// });
 
-$(document).ready(function() {
-  $('.perk-modal-close').click(function() {
-    $(".perk-modal-use").slideUp();
-  });
-});
+// $(document).ready(function() {
+//   $('.perk-modal-close').click(function() {
+//     $(".perk-modal-use").slideUp();
+//   });
+// });
