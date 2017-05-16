685c49fdc7c6b360f541f41e3af927568163445f
Frederique Petris <frederique.petris@gmail.com>
2017-05-16 23:17:12 +0200

    add popup when no perks

--- app/assets/javascripts/app/modal_business_index.js
+++ app/assets/javascripts/app/modal_business_index.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     $('.business-search-close-button-mobile, .business-search-close-button').click(function() {
-      $('#search-dashboard').val('');
+      $('#search-dashboard, #search-in-navbar-mobile, #search-in-navbar-mobile-perks').val('');
+      $('#search-in-navbar-mobile-perks').html('');
       $(".search-modal").addClass('hidden');
       $(".search-category-overlay").addClass('hidden');
       $(".business-search-index-js").addClass('hidden');
