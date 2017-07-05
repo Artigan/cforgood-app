a6520d105f468a12d79c4a274cd128309b4d29e6
Frederique Petris <frederique.petris@gmail.com>
2017-07-05 18:27:22 +0200

    change configuration of geocoder to convert IP to coordinates

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,5 +1,8 @@
 Geocoder.configure(
   lookup:    :google,
+  freegeoip: { host: "freegeoip.net" },
+  # ip_lookup: :freegeoip,
+  timeout:   20,
   api_key:   ENV['GOOGLE_API_KEY'],
   use_https: true,
   units:     :km       # :km for kilometers or :mi for mile
