fa6839b86bdb1036987ab59a2e652b56ec8ec06a
Frederique Petris <frederique.petris@gmail.com>
2017-07-17 17:36:15 +0200

    change action_mailer configuration for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "http://cforgood-staging.herokuapp.com/" }
   # config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.delivery_method = :postmark
-  config.action_mailer.postmark_settings = { api_key: ENV['POSTMARK_API_TOKEN'] }
+  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.postmark_settings = { api_key: ENV['POSTMARK_API_TOKEN'] }
 
 end
