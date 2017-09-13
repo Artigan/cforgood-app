f20a2938c2c44b50e3e536cb4b8c6d59fb995ab9
Frederique Petris <frederique.petris@gmail.com>
2017-09-14 00:44:41 +0200

    update API patch user for picture update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -48,10 +48,6 @@ class Api::V1::UsersController < Api::V1::BaseController
     else
       old_acct_id = @user.cause.acct_id
       params = user_params.except(:stripeToken)
-      puts "user_param cause : #{user_params[:cause]}"
-      puts "user_param picture : #{user_params[:picture]}"
-      puts "user_param name : #{user_params[:name]}"
-      puts "user_param stripeToken : #{user_params[:stripeToken]}"
       if !@user.update(params)
         render status: :unprocessable_entity, json: { error: @user.errors.full_messages}
       else
@@ -96,6 +92,7 @@ class Api::V1::UsersController < Api::V1::BaseController
       :city,
       :cause_id,
       :picture,
+      :remote_picture_url,
       :stripeToken)
   end
 end
