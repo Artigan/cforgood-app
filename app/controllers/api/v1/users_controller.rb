60b8f1b7f925fce4ffe91fb8931ac171e793309e
Frederique Petris <frederique.petris@gmail.com>
2017-11-29 12:17:17 +0100

    debbug facebook connect

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -21,6 +21,8 @@ class Api::V1::UsersController < Api::V1::BaseController
       params = params.merge(token: user_params[:access_token])
       params = params.merge(password: Devise.friendly_token[0,20])
       params = params.merge(picture: image["data"]["url"]) if image.present?
+      puts facebook_params
+      puts params
     end
 
     @user = User.new(params)
