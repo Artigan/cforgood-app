327969d3630b34523a0badf8e72c062a40c502b0
Fred Petris <frederique.petris@gmail.com>
2018-08-16 10:27:05 +0200

    fix for bug on zipcode in mobile app

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,14 @@ class Api::V1::UsersController < Api::V1::BaseController
   before_action :set_user, only: [ :show, :update ]
 
   def create
-    render status: :unprocessable_entity, json: { error: "Zipcode or City are required"} if !user_params[:city].present? || !user_params[:zipcode].present?
+    ##### for mobile : zipcode not required until mobile app not correct
+    params = user_params.except(:access_token)
+    if !user_params[:zipcode].present?
+      params[:zipcode] = "33000"
+      params[:city] = "Bordeaux"
+    end
+    # render status: :unprocessable_entity, json: { error: "Zipcode or City are required"} if !user_params[:city].present? || !user_params[:zipcode].present?
+    #####
     if user_params[:access_token].present?
       url = "https://graph.facebook.com/me?fields=email,picture&access_token="
       begin
@@ -16,7 +23,6 @@ class Api::V1::UsersController < Api::V1::BaseController
       end
     end
 
-    params = user_params.except(:access_token)
     if user_params[:access_token].present?
       params = params.merge(token: user_params[:access_token])
       params = params.merge(password: Devise.friendly_token[0,20])
